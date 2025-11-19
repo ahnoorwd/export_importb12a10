ec783e294975932db6a0837a4aabf392c3692e51
ahmed nur <ahmednur103308@gmail.com>
2025-11-19 23:00:15 +0600

    Added the latest product via a get api calling

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import Banner from './Banner';
+import { useLoaderData } from 'react-router';
+import Latestproductcards from '../components/Latestproductcards';
 
 const Home = () => {
+    const latestproducts = useLoaderData();
+    console.log(latestproducts);
     return (
         <div>
             <Banner></Banner>
+
+            <div><h3 className='text-center font-bold text-2xl mb-4'>Our Latest Product</h3></div>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 px-4 md:px-8 lg:px-12'>
+               {
+                latestproducts.map(singlelatestproduct=><Latestproductcards singlelatestproduct={singlelatestproduct}></Latestproductcards>)
+               }
+            </div>
         </div>
     );
 };
---
+++ src/components/Latestproductcards.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+const Latestproductcards = ({singlelatestproduct}) => {
+    return (
+        <div>
+            <div className="  mb-4 bg-white rounded-xl shadow-md hover:shadow-xl transition-all duration-300 p-4 border">
+      {/* Product Image */}
+      <div className="w-full h-48 overflow-hidden rounded-lg">
+        <img
+          src={singlelatestproduct.productImage}
+          alt={singlelatestproduct.productName}
+          className="w-full h-full object-cover hover:scale-110 transition-transform duration-300"
+        />
+      </div>
+
+      {/* Content */}
+      <div className="mt-4 space-y-1">
+        <h2 className="text-xl font-semibold text-gray-800">
+          {singlelatestproduct.productName}
+        </h2>
+
+        <p className="text-lg font-bold text-green-600">Price :${singlelatestproduct.price}</p>
+
+        <p className="text-sm text-gray-500">
+          Origin Country: <span className="font-medium">{singlelatestproduct.originCountry}</span>
+        </p>
+
+        <p className="text-sm text-gray-500">
+          Rating: ⭐ {singlelatestproduct.rating}
+        </p>
+
+        <p className="text-sm text-gray-500">
+          Available Quantity: 
+          <span className="font-medium ml-1">{singlelatestproduct.availableQuantity}</span>
+        </p>
+      </div>
+
+      {/* Button */}
+      <button
+        className="mt-4 w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition-all duration-200"
+      >
+        See Details
+      </button>
+    </div>
+        </div>
+    );
+};
+
+export default Latestproductcards;
